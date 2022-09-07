6d6357988c1d327602a36335b66cedbd2d553e33
Molnár László <garrotter@gmail.com>
2022-09-07 13:44:03 +0200

    First version

---
+++ pages/events.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container content">
+        <section class="flex">
+            <div class="right">
+                <img src="~/public/img/meditation.svg" alt="meditáció">
+            </div>
+            <div class="text">
+                <h4>Meditáció</h4>
+                <p>
+                    Minden hétfőn 18 órától
+                </p>
+            </div>
+        </section>
+        <section class="flex">
+            <div class="left">
+                <img src="~/public/img/yoga.svg" alt="jóga">
+            </div>
+            <div class="text">
+                <h4>Jóga</h4>
+                <p>
+                    Minden pénteken 18 órától<br>
+                    <em>Átadáshoz kötött gyakorlás!</em>
+                </p>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    const title = 'Események - Galambos Péter Buddhista Alapítvány'
+    
+    useHead({
+        title: title ,
+        meta: [
+            { name: 'description', content: title }
+        ]
+    })
+</script>
---
+++ pages/privacy.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container content">
+        <section>
+            <h2>Adatkezelési nyilatkozat</h2>
+            <p>
+                A Galambos Péter Buddhista Alapítvány kiemelt figyelmet fordít arra, hogy adatkezelése során az Ön érdekében az információs önrendelkezési jogról és az információszabadságról szóló 2011. évi CXII. törvény (a továbbiakban: Info.tv.), illetve az elektronikus kereskedelmi szolgáltatások, valamint az információs társadalommal összefüggő szolgáltatások egyes kérdéseiről szóló 2001. évi CVIII. törvény és az egyéb hatályos adatkezeléssel kapcsolatos jogszabályoknak megfelelően járjon el.
+            </p>
+            <p>
+                Honlapunkra látogatókat a kezelt személyes adatokról, a személyes adatok kezelése körében követett gyakorlatáról, a személyes adatok védelme érdekében tett szervezési és technikai intézkedéseiről, valamint az érintettek jogai gyakorlásának módjáról és lehetőségeiről ezúton tájékoztatjuk.
+            </p>
+            
+            <h3>Alapelveink:</h3>
+                
+            <p>
+                Személyes adatokat csak a hatályos törvényeknek megfelelően gyűjtünk, tárolunk és kezelünk. Az érdeklődőknek a róla tárolt adatokról tájékoztatást adunk, ha írásban kéri a személyes adatok törlését, akkor azt a törvényben előírt módon végrehajtjuk.
+            </p>
+            
+            <h3>Az adatkezelő megnevezése, elérhetősége:</h3>
+                
+            <p>
+                Galambos Péter Buddhista Alapítvány<br><br>
+
+                Székhely: 1025 Budapest, Verecke út 15821/4. hrsz.<br>
+                Adószám: 19328445-1-41
+            </p>
+            
+            <h3>Az adatkezelés jogalapja, meghatározása</h3>
+
+            <p>
+                A honlap révén megvalósuló adatkezelésekre az Info.tv. 5. § (1) bekezdés a) pontja alapján, a felhasználók önkéntes hozzájárulásával kerül sor. A hozzájárulást a felhasználó a honlap használatával, valamint személyes adatai önkéntes megadásával adja meg. A honlapon találhatóak olyan linkek, amelyek más weboldalakra mutatnak. Az ilyen külső honlapok adatkezelésére vonatkozóan az adott honlap adatkezelési nyilatkozatában foglaltak irányadók.
+            </p>
+
+            <h3>Kezelt adatok köre, az adatkezelés célja</h3>
+
+            <p>
+                Az adatkezelő a különböző rendezvényein történt regisztráció során a felhasználók által megadott e-mail címekre csak a felhasználó által igénybe vett szolgáltatásaihoz kapcsolódó e-maileket küld. Nem küldünk kéretlen reklámüzenetet.
+            </p>
+
+            <h3>Adattovábbítás, adatfeldolgozás, adatbiztonság</h3>
+
+            <p>
+                A tudomásunkra jutott személyes adatokat harmadik személy számára a törvényben meghatározott eseteken kívül nem adjuk át harmadik személyeknek, azokat nem tesszük közzé. Személyes adatok továbbítására kizárólag a vonatkozó törvények ilyen rendelkezése szerint, vagy az érintett hozzájárulása esetén kerülhet sor, az ott meghatározott terjedelemben.
+            </p>
+
+            <h3>A felhasználók jogai és jogérvényesítési lehetőségei</h3>
+
+            <p>
+            Az adatkezelő a felhasználó kérésére tájékoztatást ad a rá vonatkozó, általa kezelt adatokról, az adatkezelés céljáról, jogalapjáról, időtartamáról, továbbá arról, hogy kik és milyen célból kapják vagy kapták meg adatait. Az adatkezelő a kérelem benyújtásától számított 30 napon belül írásban adja meg a kért
+            tájékoztatást.
+            </p>
+            <p>
+                A felhasználó bármikor jogosult a helytelenül rögzített adatainak helyesbítését vagy azok törlését kérni. Az adatkezelő a kérelem beérkezésétől számított 7 munkanapon belül törli az adatokat, ez esetben azok nem lesznek újra helyreállíthatóak. A törlés nem vonatkozik a jogszabály alapján szükséges adatkezelésekre, azokat az adatkezelő a szükséges időtartamig megőrzi.
+            </p>
+
+            <h3>Az adatvédelmi nyilatkozat módosításának lehetősége</h3>
+
+            <p>
+                Az adatkezelő fenntartja a jogot, hogy jelen adatvédelmi nyilatkozatot a felhasználók előzetes értesítése mellett egyoldalúan módosítsa. A módosítás hatályba lépését követően a felhasználó a szolgáltatás használatával ráutaló magatartással elfogadja a módosított adatvédelmi nyilatkozatban foglaltakat.
+            </p>
+            <p>
+                A felhasználó az Info.tv., valamint a Polgári Törvénykönyv (2013. évi V. törvény) alapján bíróság előtt
+                érvényesítheti jogait, illetve panasszal a Nemzeti Adatvédelmi és Információszabadság Hatósághoz fordulhat (1125 Budapest, Szilágyi Erzsébet fasor 22/C. Postacím: 1530 Budapest, Postafiók: 5., további információ: www.naih.hu).
+            </p>
+        </section>
+    </div>
+</template>
+
+<script setup>
+    const title = 'Adatkezelési nyilatkozat - Galambos Péter Buddhista Alapítvány'
+
+    useHead({
+        title: title ,
+        meta: [
+            { name: 'description', content: title }
+        ]
+    })
+</script>
